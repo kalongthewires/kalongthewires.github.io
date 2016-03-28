e3a3e0b0eadd8b2f8dc320209618f2d3a74e97e1
Kim Hamper <kim.hamper@gmail.com>
2016-03-28 08:18:03 -0400

    Refactor date countdown widget

--- js/DateCountdown.js
+++ js/DateCountdown.js
@@ -1,24 +1,25 @@
+'use strict';
+
 function DateCountdown (options) {
 	this.element = document.querySelector(options.element);
 	this.endDate = options.endDate;
-
-	this.displayRemainingDays();
 }
 
 DateCountdown.prototype = {
 	constructor: DateCountdown,
+	init: function () {
+		var days = this.calculateRemainingDays();
+
+		this.displayRemainingDays(days);
+	},
 	calculateRemainingDays: function () {
 		var today = new Date(),
 			millisPerDay = 24 * 60 * 60 * 1000;
 
 		return Math.round(Math.abs((today - this.endDate) / millisPerDay));
 	},
-	displayRemainingDays: function () {
-		var days = this.calculateRemainingDays();
-
-		days = document.createTextNode(days)
+	displayRemainingDays: function (days) {
+		days = document.createTextNode(days);
 		this.element.appendChild(days);
-
-		return days;
 	}
 };
