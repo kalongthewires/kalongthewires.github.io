8f85c1f8d131b255a1ab6d5558b71c35c7a1a308
Kim Hamper <kim.hamper@gmail.com>
2026-05-12 09:25:55 -0400

    Clean up

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,32 +1,33 @@
 import eleventyNavigationPlugin from "@11ty/eleventy-navigation";
 
+const groupByMonth = (values) => {
+  const grouped = values.reduce((acc, item) => {
+    const displayMonth = item.page.date.toLocaleString("default", {
+      month: "long",
+    });
+    const numericMonth = item.page.date.toLocaleString("default", {
+      month: "numeric",
+    });
+
+    return [
+      ...(acc.filter((group) => group.numericMonth !== numericMonth) || []),
+      {
+        name: displayMonth,
+        numericMonth,
+        items: [
+          ...(acc.find((group) => group.numericMonth === numericMonth)?.items ||
+            []),
+          item,
+        ],
+      },
+    ].sort((a, b) => b.numericMonth - a.numericMonth);
+  }, []);
+
+  return grouped;
+};
+
 export default function (eleventyConfig) {
   eleventyConfig.addPlugin(eleventyNavigationPlugin);
 
-  eleventyConfig.addFilter("groupByMonth", (values) => {
-    const grouped = values.reduce((acc, item) => {
-      const displayMonth = item.page.date.toLocaleString("default", {
-        month: "long",
-      });
-      const numericMonth = item.page.date.toLocaleString("default", {
-        month: "numeric",
-      });
-
-      return [
-        ...(acc.filter((group) => group.numericMonth !== numericMonth) || []),
-        {
-          name: displayMonth,
-          numericMonth,
-          items: [
-            ...(acc.find((group) => group.numericMonth === numericMonth)
-              ?.items || []),
-            item,
-          ],
-        },
-      ].sort((a, b) => b.numericMonth - a.numericMonth);
-    }, []);
-
-    console.log(grouped);
-    return grouped;
-  });
+  eleventyConfig.addFilter("groupByMonth", groupByMonth);
 }
